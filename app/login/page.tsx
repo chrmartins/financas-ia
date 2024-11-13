14ed3be517083062c62ca8245e042c5f3621be9e
Christian Martins <cmts2010@gmail.com>
2024-11-13 00:46:35 -0300

    fix: tele de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { BarChart, LogInIcon } from "lucide-react";
 import { SignInButton } from "@clerk/nextjs";
@@ -11,40 +10,24 @@ const LoginPage = async () => {
     redirect("/");
   }
   return (
-    <div className="grid h-full min-h-screen grid-cols-2">
+    <div className="grid h-full min-h-screen">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <div className="flex items-center justify-center space-x-4 p-12">
+        <div className="flex items-center justify-center space-x-4 pb-16">
           <BarChart className="text-green-600" size={36} strokeWidth={4} />{" "}
           <h1 className="text-4xl font-bold">SEControle</h1>
         </div>
-        {/* <Image
-          src="/logo.svg"
-          width={173}
-          height={39}
-          alt="Finance AI"
-          className="mb-8"
-        /> */}
-        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="mb-8 text-muted-foreground">
+        <h1 className="mb-3 text-4xl font-bold pb-6">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground pb-6">
           A SEControle é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
-          <Button variant="outline">
+          <Button className="p-8 border-green-400" variant="outline">
             <LogInIcon className="mr-2" />
             Fazer login ou criar conta
           </Button>
         </SignInButton>
-      </div>
-
-      <div className="relative h-full w-full">
-        <Image
-          src="/login.png"
-          alt="Faça login"
-          fill
-          className="object-cover"
-        />
       </div>
     </div>
   );
