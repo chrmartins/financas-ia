ed5dd7234e0ec9c53373ab40ef122ed42830c855
Christian Martins <cmts2010@gmail.com>
2025-03-07 13:45:15 -0300

    fix: fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,11 +11,12 @@ import LastTransactions from "./_components/last-transactions";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 import AiReportButton from "./_components/ai-report-button";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+type PageProps = {
+  params: { [key: string]: string | string[] | undefined }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+export default async function Home({ searchParams = {} }: PageProps) {
   const month = searchParams.month as string;
   const authResult = await auth();
   
